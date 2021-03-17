2969cc7aa8622507b7f9a254732f3ca919d4ff6c
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-03-17 20:55:32 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Nav from './Nav/Nav'
 import Section1 from './Section1/Section1'
+import Section2 from './Section2/Section2'
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Section1 />
+      <Section2 />
     </div>
   );
 }
---
+++ src/Section2/Section2.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Section2.css'
+const Section2 = () => {
+    return (
+        <div className='section2'>
+            <div className='title2'>
+                <h2>iPhone 11 Pro</h2>
+                <h3>From twenty nine dollars and twelve cents per month for twenty four months or six hundred ninety nine. <br />
+                    Buy directly from Apple with special carrier offers.</h3>
+                <p>From $29.12/mo. for 24 mo. or $699 before trade‑in.</p>
+            </div>
+            <div className='link2'>
+                <p>Learn more</p>
+                <p>Buy</p>
+            </div>
+        </div>
+    )
+}
+
+export default Section2
