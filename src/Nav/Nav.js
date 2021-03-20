887a55089af0e7a97e8f064633fc1ca0b2dddfcb
Darasimi-okeowo <okeowoadrasimidavid19@gmail.com>
2021-03-20 17:55:18 +0100

    last-commit

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -3,8 +3,8 @@ import './Nav.css'
 import { AiFillApple } from 'react-icons/ai';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { BsFillBagFill } from 'react-icons/bs';
-import { ImCross } from 'react-icons/im';
-import { CgMathEqual } from 'react-icons/cg';
+// import { ImCross } from 'react-icons/im';
+// import { CgMathEqual } from 'react-icons/cg';
 
 
 const Nav = () => {
@@ -16,19 +16,29 @@ const Nav = () => {
     }
 
     return (
-        <header>
+        <header className={`${toggle && 'active-header'}`}>
             <div className='container'>
                 <div className='nav'>
                     <ul className='nav-list nav-list-mobile'>
                         <li className='nav-item'>
                             <div onClick={togglehandler} className={`mobile-menu `}>
-                                <span className={toggle ? <ImCross /> : <CgMathEqual />}><ImCross /></span>
+                                {/* I think you want to achieve this */}
+                                {/* {toggle && <span className=''><ImCross /></span>} */}
+                                {/* {!toggle && <span className=''><ImCross /></span>} */}
+                                {/* above is also the same as  */}
+                                {/* {toggle 
+                                    ? <span className=''><ImCross /></span> 
+                                    : <span className=''><CgMathEqual /></span>
+                                } */}
+                                {/* Using class to achieve it would look like this */}
+                                <span className={`line line-top ${toggle && 'active-top'}`}></span>
+                                <span className={`line line-bottom ${toggle && 'active-bottom'}`}></span>
                             </div>
                         </li>
                         <li className='nav-item nav-link nav-link-apple'><AiFillApple /></li>
-                        <li className='nav-item nav-link nav-link-bag'><BsFillBagFill /></li>
+                        <li className={`nav-item nav-link nav-link-bag ${toggle && 'active-nav-link-bag'}`}><BsFillBagFill /></li>
                     </ul>
-                    <ul className='nav-list nav-list-larger'>
+                    <ul className={`nav-list nav-list-larger ${toggle && 'active-nav-list-larger'}`}>
                         <li className='nav-item nav-item-hidden nav-link nav-link-apple'><AiFillApple /></li>
                         <li className='nav-item nav-link'>Mac</li>
                         <li className='nav-item nav-link'>iPad</li>
